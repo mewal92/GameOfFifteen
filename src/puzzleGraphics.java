14652d125d72519c9f7d5bc619cf0677f5b94033
Melinda <melinda_walter@hotmail.com>
2022-10-29 23:52:15 +0200

    initial

--- src/puzzleGraphics.java
+++ src/puzzleGraphics.java
@@ -3,62 +3,58 @@ import javax.swing.*;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.plaf.metal.MetalBorders;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-    public class puzzleGraphics extends JFrame implements ActionListener, MouseListener {
-        private JPanel gamePanel;
-        private JPanel panel;
-        private JFrame main;
-        private JSeparator separator;
+    public class puzzleGraphics extends JFrame {
+
         private BufferedImage img;
+        JPanel gamePanel;
+        JFrame frame;
+        JButton newGame;
+        JButton []buttons;
+        JButton emptyButton;
+        JCheckBox checkBox;
 
         puzzleGraphics(){
-            panel = new JPanel();
-
+            frame = new JFrame();
             gamePanel = new JPanel();
-            main = new JFrame();
-            separator = new JSeparator(1);
+            buttons= new JButton[16];
+            newGame= new JButton("Nytt Spel");
+            checkBox = new JCheckBox("Test mode");
 
             try {
-                img = ImageIO.read(new File("src/bk.jpg"));
-                main.setContentPane(new JLabel(new ImageIcon(img)));
-                main.setLayout(new FlowLayout());
-                main.getContentPane().add(gamePanel);
-                main.getContentPane().add(separator);
-                main.getContentPane().add(newGame);
-
+                img = ImageIO.read(new File("bk.jpg"));
+                frame.setContentPane(new JLabel(new ImageIcon(img)));
+                frame.setLayout(new FlowLayout());
+                frame.getContentPane().add(gamePanel);
+               frame.getContentPane().add(newGame);
+                frame.getContentPane().add(checkBox);
 
                     gamePanel.setBorder(new MetalBorders.InternalFrameBorder());
                     gamePanel.setBorder(new SoftBevelBorder(1, Color.blue, Color.gray));
-                    gamePanel.setSize(460, 360);
+                    gamePanel.setSize(400, 400);
                     gamePanel.setLayout(new GridLayout(4, 4));
-                    gamePanel.setBackground(Color.darkGray);
 
-                gamePanel.add(buttons[i]);
-                gamePanel.add(emptyButton);
-
-
-                    main.setTitle("Game of 15");
-                    main.setVisible(true);
-                    main.setLocationRelativeTo(null);
-                    main.setSize(460, 500);
-                    main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                    //läser in knappar från 1-15 när man startar programmet
+                for (int i = 1; i <buttons.length; i++) {
+                    buttons[i] = new JButton(String.valueOf(i));
+                    buttons[i].setSize(50,50);
+                    buttons[i].setFont(new Font("Tahoma", Font.PLAIN, 25));
+                    gamePanel.add(buttons[i]);
+                }
+                    frame.setTitle("Game of 15");
+                    frame.setVisible(true);
+                    frame.setLocationRelativeTo(null);
+                    setLocationRelativeTo(null);
+                    frame.setSize(460, 350);
+                    frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
             } catch (IOException exp) {
                 exp.printStackTrace();
             }
-        }
-
-
-        public static void main(String[] args) {
-            puzzleGraphics pg = new puzzleGraphics();
         }
     }
 
